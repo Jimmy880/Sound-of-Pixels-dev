ea198514d15278b4e2d1167b636251ce54e21d87
wangzhenyu2168@gmail.com <wzy78757496>
2020-08-17 12:52:32 +0800

    init

--- models/criterion.py
+++ models/criterion.py
@@ -8,7 +8,7 @@ class BaseLoss(nn.Module):
         super(BaseLoss, self).__init__()
 
     def forward(self, preds, targets, weight=None):
-        N = len(preds) # shape = mix, batch, len
+        N = len(preds)  # shape = mix, batch, len
         if weight is None:
             weight = preds[0].new_ones(1)
 
@@ -64,11 +64,12 @@ def sisnr(x, s, eps=1e-8):
     s_zm = s - torch.mean(s, dim=-1, keepdim=True)
     t = torch.sum(
         x_zm * s_zm, dim=-1,
-        keepdim=True) * s_zm / (l2norm(s_zm, keepdim=True)**2 + eps)
+        keepdim=True) * s_zm / (l2norm(s_zm, keepdim=True) ** 2 + eps)
     if torch.isnan(20 * torch.log10(eps + l2norm(t) / (l2norm(x_zm - t) + eps))).any():
-        print(eps, t, x, s, l2norm(t), l2norm(x_zm - t), (l2norm(x_zm - t)+eps))
+        print(eps, t, x, s, l2norm(t), l2norm(x_zm - t), (l2norm(x_zm - t) + eps))
         raise ValueError
     return 20 * torch.log10(eps + l2norm(t) / (l2norm(x_zm - t) + eps))
+
 
 class SISNRLoss(BaseLoss):
     def __init__(self):
@@ -76,6 +77,7 @@ class SISNRLoss(BaseLoss):
 
     def _forward(self, pred, target, weight):
         return - sisnr(pred, target)
+
 
 class UPITLoss(nn.Module):
     def __init__(self):
@@ -90,22 +92,22 @@ class UPITLoss(nn.Module):
             # for one permute, return `[batch]`, on this permute
             z = sum(
                 [sisnr(ests[s], refs[t])
-                for s, t in enumerate(permute)]) / len(permute)
-                # average the value
+                 for s, t in enumerate(permute)]) / len(permute)
+            # average the value
             # print(z)
             return z
 
         # P x N
         N = egs[0].size(0)
         from itertools import permutations
-        sisnr_mat = torch.stack( # `[permute, batch]`
+        sisnr_mat = torch.stack(  # `[permute, batch]`
             [sisnr_loss(p) for p in permutations(range(num_spks))])
         # print(sisnr_mat)
-        max_perutt, _ = torch.max(sisnr_mat, dim=0) # find max-permute opt, return `[batch]`
+        max_perutt, _ = torch.max(sisnr_mat, dim=0)  # find max-permute opt, return `[batch]`
 
         if torch.isnan(-torch.sum(max_perutt) / N):
             print(N, max_perutt, sisnr_mat)
             raise ValueError
 
         # si-snr
-        return -torch.sum(max_perutt) / N # make average on spks
+        return -torch.sum(max_perutt) / N  # make average on spks
